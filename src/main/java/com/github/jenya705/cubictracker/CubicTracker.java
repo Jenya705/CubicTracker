bfd289a8dd581b35a825cca44081ffb87cf9ba67
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2022-05-05 20:23:24 +0300

    Initial commit

---
+++ src/main/java/com/github/jenya705/cubictracker/CubicTracker.java
@@ -0,0 +1,16 @@
+package com.github.jenya705.cubictracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CubicTracker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
